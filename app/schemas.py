ab57b04036c54a11b8a663e8b9bc6d1816d6e3fb
parmarpalak2000@gmail.com <palak.parmar@tntra.io>
2022-01-10 17:04:07 +0530

    [ADD] blogapp using flask restplus

---
+++ app/schemas.py
@@ -0,0 +1,28 @@
+from app.models import User, Article, Comment
+from . import ma
+
+class UserSchema(ma.Schema):
+    class Meta:
+        model = User
+        fields = ("user_id", "username" )
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+class ArticleSchema(ma.Schema):
+    class Meta:
+        model = Article
+        fields = ("title", "content", "date", "u_id", )
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
+
+
+class CommentSchema(ma.Schema):
+    class Meta:
+        model = Comment
+        fields = ("user_id", "article_id", "description")
+        
+comment_schema = CommentSchema()
+comments_schema = CommentSchema(many=True)
